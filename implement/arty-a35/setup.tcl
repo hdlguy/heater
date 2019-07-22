8be46d4ad4f8e196de67da81906a3f055af5c116
hdlguy <hdlguy@hotmail.com>
2019-07-22 08:16:24 -0400

    compiled arty with 2019.1

--- implement/arty-a35/setup.tcl
+++ implement/arty-a35/setup.tcl
@@ -3,8 +3,8 @@ close_project -quiet
 file delete -force proj.xpr *.os *.jou *.log proj.srcs proj.cache proj.runs
 #
 create_project -force proj 
-#set_property part xc7a50tftg256-1 [current_project]
-set_property board_part digilentinc.com:arty-a7-35:part0:1.0 [current_project]
+set_property part xc7a35ticsg324-1L [current_project]
+#set_property board_part digilentinc.com:arty-a7-35:part0:1.0 [current_project]
 set_property target_language verilog [current_project]
 set_property default_lib work [current_project]
 load_features ipintegrator
