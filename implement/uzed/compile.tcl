c6a39a548f5b8b4f6ba913e0050293c134cc2d3c
hdlguy <hdlguy@hotmail.com>
2019-11-09 09:04:17 -0500

    trying different bootgen command

--- implement/uzed/compile.tcl
+++ implement/uzed/compile.tcl
@@ -32,7 +32,8 @@ write_bitstream -force ./results/top.bit
 close_project
 
 #exec bootgen -image bitstream.bif -arch zynq -o ./results/top.bit.bin -w
-exec bootgen -image bitstream.bif -arch zynqmp -process_bitstream bin -o ./results/top.bit.bin -w
+#exec bootgen -image bitstream.bif -arch zynqmp -process_bitstream bin -o ./results/top.bit.bin -w
+exec bootgen -image bitstream.bif -arch zynqmp -o ./results/top.bit.bin -w
 
 
 
