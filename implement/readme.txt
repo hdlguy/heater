3c46b99af67223aeda5959a8cb0457e281696aee
pete <peter.dudley@precisionplanting.com>
2021-07-28 14:23:11 -0400

    compiles for Kria

--- implement/readme.txt
+++ implement/readme.txt
@@ -1,6 +1,12 @@
-This folder holds the build scripts for the FPGA designs.  There is a folder to build for
-the MicroZed (or ZedBoard) with a Zynq 7020 part. Another folder is provided for the Avnet
-Artix 50t eval board.
+This folder holds the build scripts for the FPGA designs.  Several folders hold the build files for different targets.
+
+    artix-50t - The Avnet Artix-50t board.
+    arty-a35  - The Digilent Arty A7 a35 board.
+    kria_kv260 - The Xilinx Kria KV260.
+    picozed_7030 - The Avnet Picozed 7030 SOM on the Avnet PCIe Carrier.
+    uzed - The Avnet Ultrazed 3-EG board on the Avnet IO Carrier card.
+    zcu104 - The Xilinx ZCU104 Zynq Ultrascale Eval board.
+    zed - The original Zedboard with Zynq 7020.
 
 These FPGA designs have been built and tested with Vivado 2016.4.
 
---
+++ source/kria_top.v
@@ -0,0 +1,31 @@
+//
+module top (
+    input   logic       reset
+);
+
+    localparam Nchan = 32;
+    localparam Nsrl  = 20;
+    localparam Nbram = 4;
+    localparam Ndsp  = 30;
+    localparam Npipe = 90;
+
+    logic axiclk, clk;
+    clk_wiz_uzed clk_wiz_inst (.clk_out(clk), .clk_in100(axiclk)); 
+
+    logic [Nchan-1:0] heater_error, heater_err_clear, heater_enable;
+
+    genvar i;  
+    generate  for (i=0; i < Nchan; i++) begin: gen_code_label  
+        heater #(.Nsrl(Nsrl), .Nbram(Nbram), .Ndsp(Ndsp), .Npipe(Npipe)) heater_inst(.clk(clk), .enable(heater_enable[i]), .error(heater_error[i]), .err_clear(heater_err_clear[i]));
+    end  endgenerate 
+    
+    // the ZynqMP.
+    kria_system system_i(
+        .axi_aclk            (axiclk),        
+        .GPIO_0_out_tri_o    (heater_enable),
+        .GPIO2_0_in_tri_i    (heater_error),
+        .GPIO_1_out_tri_o    (heater_err_clear)
+    );
+
+endmodule
+
