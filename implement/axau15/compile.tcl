233bba9536aa054556fa1ad210f4fb0741a882b8
Your Name <you@example.com>
2026-02-15 07:45:17 -0500

    bitstream generation

--- implement/axau15/compile.tcl
+++ implement/axau15/compile.tcl
@@ -9,19 +9,20 @@ update_compile_order -fileset sources_1
 reset_run synth_1
 launch_runs synth_1 -jobs 8
 wait_on_run synth_1
-
+#launch_runs impl_1 -jobs 8 -do_step write_bitstream
 launch_runs impl_1 -jobs 8
 wait_on_run impl_1
 
 open_run impl_1
-write_checkpoint     -force ./results/post_route.dcp
 write_debug_probes   -force ./results/top.ltx
-write_hw_platform -fixed -force -file   ./results/top.xsa
 report_timing_summary -file ./results/timing.rpt
 report_utilization    -file ./results/utilization.rpt
 
 set_property BITSTREAM.GENERAL.COMPRESS TRUE [get_designs impl_1]
-write_bitstream -verbose -force -bin_file ./results/top.bit
+write_bitstream -verbose -force ./proj.runs/impl_1/top.bit
+write_hw_platform -fixed -force -file ./results/top.xsa
+
+file copy ./proj.runs/impl_1/top.bit ./results/
 
 close_project
 
